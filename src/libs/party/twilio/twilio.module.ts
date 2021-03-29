bffdc4a2f95b2a768a84f269c9f13893afb5f257
lanl0rd <k1ng@glass.earth>
2021-03-29 17:51:16 -0400

    grapes

---
+++ src/libs/party/twilio/twilio.module.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component
+({
+  selector: 'party-paypal-element',
+  templateUrl: './paypal.element.html',
+  styleUrls: ['./paypal.element.scss']
+})
+export class PartyPaypalElement
+{
+
+
+}
