9774a10baead011f43b01b98a72dda80b675bbfe
lanl0rd <k1ng@glass.earth>
2021-03-30 00:22:19 -0400

    funny

---
+++ docs/15-es2015.f7af6b8adb9c4a05d424.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{sdAV:function(n,t,o){"use strict";o.r(t),o.d(t,"PartyFeatureModule",(function(){return u}));var r=o("ofXK"),e=o("fXoL");let u=(()=>{class n{}return n.\u0275mod=e.Hb({type:n}),n.\u0275inj=e.Gb({factory:function(t){return new(t||n)},imports:[[r.b]]}),n})()}}]);
